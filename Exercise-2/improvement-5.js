fda755011ca1e51eb485907e913ba25b859fd630
Seacrs <shemachris072@gmail.com>
2026-04-18 18:49:43 +0200

    refactor: use recursive approach for calculateFactorial

--- Exercise-2/improvement-5.js
+++ Exercise-2/improvement-5.js
@@ -1,13 +1,10 @@
 // Function 5
 
-function calculateFactorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    var factorial = 1;
-    for (var i = 2; i <= n; i++) {
-      factorial *= i;
-    }
-    return factorial;
-  }
+/** Calculates Factorial of a number
+ * @param {number} n 
+ * @returns {number}
+ */
+const calculateFactorial = (n = 0) => {
+  if (n === 0 || n === 1) return 1;
+  return n * calculateFactorial(n-1);
 }
