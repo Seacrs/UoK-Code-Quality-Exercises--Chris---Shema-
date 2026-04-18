51ac10f1c308ff98461b393cd4362a36e03a32a5
Seacrs <shemachris072@gmail.com>
2026-04-18 21:07:45 +0200

    feat: implement recursive array sum function

--- Exercise-3/function-3.js
+++ Exercise-3/function-3.js
@@ -1,1 +1,11 @@
 // Implement a JavaScript function to find the sum of all elements in an array recursively.
+
+/**
+ * Finds sum of all elements in an array recursively
+ * @param {number[]} array 
+ * @returns {number}
+ */
+const addElements = (array) => {
+    if(array.length === 0) return 0;
+    return array[0] + addElements(array.slice(1))
+}
