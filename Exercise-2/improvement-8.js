5deaa2f65da0b0263c6b2bbd13893fdcc53ca4c8
Seacrs <shemachris072@gmail.com>
2026-04-18 19:05:33 +0200

    refactor: simplify discount calculation using tenary operator

--- Exercise-2/improvement-8.js
+++ Exercise-2/improvement-8.js
@@ -1,11 +1,7 @@
 // Function 8
 
-function discount(totalamount) {
-  var disc;
-  if (totalamount > 100) {
-    disc = totalamount * 0.1;
-  } else {
-    disc = totalamount * 0.05;
-  }
-  return disc;
-}
+/**
+ * @param {number} totalAmount
+ * @returns {number}
+ */
+const discount = (totalAmount) => totalAmount > 100 ? totalAmount * 0.1 : totalAmount * 0.05;
