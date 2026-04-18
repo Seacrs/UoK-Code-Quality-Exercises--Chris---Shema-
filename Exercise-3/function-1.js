ec021f59bb34ff74e29ff762d9c5cb9b71d74c46
Seacrs <shemachris072@gmail.com>
2026-04-18 20:36:21 +0200

    feat: add array depth detection and flattening utility

--- Exercise-3/function-1.js
+++ Exercise-3/function-1.js
@@ -1,2 +1,17 @@
 // Write a function(s) that checks the level of nesting of an array. 
 // Then, use that function to flatten the array into a single-dimensional array.
+
+const getDepth = (array) => {
+    let maxDepth = 0;
+    array.forEach(item => {
+        if(Array.isArray(item)){
+            maxDepth = Math.max(maxDepth, getDepth(item));
+        }
+    });
+    return maxDepth + 1;
+}
+
+const flatten = (array) => {
+    const maxDepth = getDepth(array);
+    return array.flat(maxDepth);
+}
