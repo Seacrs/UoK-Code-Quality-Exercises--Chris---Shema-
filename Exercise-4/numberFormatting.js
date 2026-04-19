96fffe966985df115877c2c6562ae75d5d3f0ca2
Seacrs <shemachris072@gmail.com>
2026-04-19 19:31:47 +0200

    feat: implement number formatting with thousand separators

--- Exercise-4/numberFormatting.js
+++ Exercise-4/numberFormatting.js
@@ -5,3 +5,20 @@
 // For n = 100000 the function should return '100,000';
 // For n = 5678545 the function should return '5,678,545';
 // for n = -420902 the function should return '-420,902'.
+
+function numberFormat(number){
+    const isNegative = number < 0;
+    const str = String(Math.abs(number)).split("").reverse().join("");
+    const arr = [];
+
+    for (let i = 0; i < str.length; i += 3){
+        arr.push(str.slice(i, i + 3));
+    }
+
+    const result = arr
+        .map(s => s.split("").reverse().join(""))
+        .reverse()
+        .join(",")
+
+    return isNegative ? "-" + result : result;
+}
