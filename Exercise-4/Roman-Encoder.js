2fff08507f4d1b80e9fdbd5ee43c20c7bd0ae48b
Seacrs <shemachris072@gmail.com>
2026-04-19 18:34:51 +0200

    feat: implement Roman numeral encoder

--- Exercise-4/Roman-Encoder.js
+++ Exercise-4/Roman-Encoder.js
@@ -1,17 +1,23 @@
-// Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning a string containing the Roman Numeral representation of that integer.
+// Create a function taking a positive integer between 1 and 3999 (both included) 
+// as its parameter and returning a string containing the 
+// Roman Numeral representation of that integer.
 
-// Modern Roman numerals are written by expressing each digit separately starting with the leftmost digit and skipping any digit with a value of zero. There cannot be more than 3 identical symbols in a row.
+// Modern Roman numerals are written by expressing each digit separately 
+// starting with the leftmost digit and skipping any digit with a value of zero. 
+// There cannot be more than 3 identical symbols in a row.
 
 // In Roman numerals:
 
 // 1990 is rendered: 1000=M + 900=CM + 90=XC; resulting in MCMXC.
 // 2008 is written as 2000=MM, 8=VIII; or MMVIII.
 // 1666 uses each Roman symbol in descending order: MDCLXVI.
+
 // Example:
 
 //    1 -->       "I"
 // 1000 -->       "M"
 // 1666 --> "MDCLXVI"
+
 // Help:
 
 // Symbol    Value
@@ -22,3 +28,24 @@
 // C          100
 // D          500
 // M          1,000
+
+function solution(number) {
+    const symbol = {
+        1 : "I",  4 : "IV", 5: "V", 9: "IX", 10 : "X", 
+        40 : "XL", 50 : "L", 90 : "XC", 100 : "C", 400 : "CD",
+        500 : "D", 900 :  "CM",  1000 : "M"
+    };
+
+    let num = number;
+    const keys = Object.keys(symbol).map(Number);
+    let roman = "";
+
+    for(let i = keys.length - 1; i >= 0 ; i--){
+        while(keys[i] <= num){
+            num -= keys[i];
+            roman += symbol[keys[i]];
+        }
+    }
+
+    return roman;
+}
